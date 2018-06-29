ff53a771ca62f0b357583602f219b12fcec9792b
reganbp <reganbp@gmail.com>
2018-06-29 10:09:26 -0400

    Add curl scripts for authentication
    - Curl scripts for sign up/in/out and change password
    - Tested and working

---
+++ curl-scripts/sign-in.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+curl "http://localhost:4741/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}"\
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
